e338a331707f19bf4b1277dba85da52f72f44d2d
am-anshuman <anshumansahu0310@gmail.com>
2025-03-04 07:17:25 +0530

    BullMQ based Redis queue infra up and running

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import serverConfig from "./config/server.config";
 import apiRouter from "./routes";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
+import sampleWorker from "./workers/sampleWorker";
 
 const app = express();
 
@@ -8,4 +10,20 @@ app.use('/api', apiRouter);
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at port ${serverConfig.PORT}`);
+
+    sampleWorker('SampleQueue');
+    
+    sampleQueueProducer('SampleJob', {
+        name: "Aman",
+        company: "Inovaare",
+        position: "Associate Product Engineer",
+        location: "BBSR"
+    }, 2);
+
+    sampleQueueProducer('SampleJob', {
+        name: "Anshuman",
+        company: "Inovaare",
+        position: "Associate Product Engineer",
+        location: "BBSR"
+    }, 1);
 })
